9beb97949bd1e9cf2ebbf69d06314664e30f55c0
bsucs <122378640+bsucs@users.noreply.github.com>
2024-09-01 19:28:07 -0400

    intial

---
+++ Magic.js
@@ -0,0 +1,30 @@
+const magic = [
+    {
+      Color:'Red',
+      Magic8:'Yes'
+    },
+    {
+        Color:'Black',
+      Magic8:'Not likely'
+    },
+    {
+      Color:'Blue',
+      Magic8:'No'
+    },{
+        Color:'Green',
+      Magic8:'Maybe'
+    },
+    {
+        Color:'Purple',
+      Magic8:'Unlikely'
+    },
+    {
+        Color:'Gray',
+      Magic8:'No'
+    }
+    
+
+
+];
+let answerButton = document.createElement("button")
+answerButton.innerHTML = magic[Magic8[1]]
